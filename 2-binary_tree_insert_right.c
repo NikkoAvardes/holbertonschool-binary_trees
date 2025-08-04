c13073e2d4b4ea7024655ad29c19dec04d4f735d
NikkoAvardes <11460@holbertonstudents.com>
2025-08-04 18:39:46 +0200

    Correction de la vérification de la variable 'parent' dans les fonctions d'insertion à gauche et à droite

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,13 +10,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
 
 	binary_tree_t *new;
 
 	new = binary_tree_node(parent, value);
 
-	if (parent == NULL)
-		return (NULL);
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;
