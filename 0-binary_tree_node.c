2fb9f57a5c4f06d854be886ebbc39009c56f4042
NikkoAvardes <11460@holbertonstudents.com>
2025-08-04 14:08:09 +0200

    Correction de la documentation et des noms de variables dans la fonction de création de nœud d'arbre binaire

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_Node - Creates a binary tree Node
- * @parent: Pointer to the parent Node
- * @value: Value to store in the new Node
- * @Node: A binary tree Node structure
- * Return: Pointer to the newly created Node, or NULL on failure
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ * @binary_tree_node - Creates a binary tree node
+ * Return: Pointer to the newly created node, or NULL on failure
  */
-binary_tree_t *binary_tree_Node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (newnode == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-	newnode->n = value;
-	newnode->parent = parent;
-	newnode->left = NULL;
-	newnode->right = NULL;
-	return (newnode);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 
 }
