2a5266d470a5b4e7518668cfe17cd969fd276475
Moha-A1 <11469@holbertonstudents.com>
2025-08-05 11:26:37 +0200

    task 17

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -38,6 +40,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif
 
