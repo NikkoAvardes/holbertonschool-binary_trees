526324ba4091ef44d1afafe582cac1ab41e920bf
Moha-A1 <11469@holbertonstudents.com>
2025-08-05 10:00:53 +0200

    push

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -18,4 +18,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	/* Recursively count leaves in left and right subtrees */
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-} 
+}
+
